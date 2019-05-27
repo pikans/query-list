771b8f0f0d2d6133bc7bfb015ab5a1214f2d421b
Jay Kruer <jaykru@protonmail.ch>
2019-05-26 22:57:54 -0400

    Parameterize list query program over lists :P

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ import (
 	"fmt"
 	"github.com/pikans/mealplan/moira"
 	"log"
+	"os"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Printf("Usage: %s [list to query]\n", os.Args[0])
+	}
 	s, err := moira.GetMoiraNFSGroupMembers("pika-private")
 	if err != nil {
 		log.Print(err)
